4420665d579ecad9f5e3304c822f07fc0f601dc9
Shaun Harker <sharker@fen.hpc.rutgers.edu>
2015-06-28 19:06:35 -0400

    cluster submission

---
+++ cluster/slurm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -n 10                 # One task
+#SBATCH -c 1                 # One cpu per task
+#SBATCH -N 1                 # Minimum one node
+#SBATCH -t 0-00:05           # Runtime in D-HH:MM
+#SBATCH -p main              # Partition to submit to
+#SBATCH --mem-per-cpu=4000   # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH -o hostname.out      # File to which STDOUT will be written
+#SBATCH -e hostname.err      # File to which STDERR will be written
+#SBATCH --mail-type=ALL      # Type of email notification-BEGIN,END,FAIL,ALL
+#SBATCH --mail-user=sharker81@gmail.com #Email to which notifications will be sent
+
+mpiexec "$@"
