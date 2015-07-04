28a4c27b5b7d6203c27c6c7d2bd1da6bf72ff1c4
Shaun Harker <sharker81@gmail.com>
2015-07-04 13:30:33 -0400

    added test

---
+++ tests/tests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+SHELL_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd $SHELL_DIR
+tar xvfz ./data.tar.gz
+sed 's|REPLACEME|'${SHELL_DIR}'|g' data/sample.json > ./sample.json
+./trial.sh 1.0 1.0
+./trial.sh 100.0 1.0
+./trial.sh 1.0 inf
+./trial.sh 10.0 inf
