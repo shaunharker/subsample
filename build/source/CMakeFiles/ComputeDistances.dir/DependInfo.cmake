99f82b59f231764ba677dfd07c0f2409f8eef4ee
Rachel Levanger <rachel.levanger@gmail.com>
2015-11-06 16:16:06 -0500

    Shaun fixed CMakeLists.txt for proper flags/includes.

--- build/source/CMakeFiles/ComputeDistances.dir/DependInfo.cmake
+++ build/source/CMakeFiles/ComputeDistances.dir/DependInfo.cmake
@@ -11,6 +11,7 @@ set(CMAKE_CXX_COMPILER_ID "Clang")
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
   ".././include"
+  ".././include/persistence/AuctionDistances/ann/include"
   "/usr/local/include"
   "/opt/local/include"
   "/usr/local/Cellar/open-mpi/1.10.0/include"
