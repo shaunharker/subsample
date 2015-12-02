ea63b7d60f16612378f6eb0e405c3bd5eab4ca42
Rachel Levanger <rachel.levanger@gmail.com>
2015-12-02 10:00:31 -0500

    Cleaning up.

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/birdbrain/Documents/Research/Code/clean/subsample
-# Build directory: /Users/birdbrain/Documents/Research/Code/clean/subsample/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test "/Users/birdbrain/Documents/Research/Code/clean/subsample/tests/tests.sh")
-subdirs(include/persistence/approximatedistances)
-subdirs(source)
