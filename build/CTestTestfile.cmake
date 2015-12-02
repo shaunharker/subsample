ab7ddda710dc27579f37c4969ee44ba165c3fde3
Rachel Levanger <rachel.levanger@gmail.com>
2015-12-02 09:49:38 -0500

    Removing build files.

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/birdbrain/Documents/Research/Code/subsample
-# Build directory: /Users/birdbrain/Documents/Research/Code/subsample/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test "/Users/birdbrain/Documents/Research/Code/subsample/tests/tests.sh")
-subdirs(source)
